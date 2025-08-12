97205b6e99c0ea2ad0d5ae4bfe17a40d40a11ffc
jhonnr9 <4.jhones@gmail.com>
2025-08-11 21:26:58 -0300

    entt

--- src/main.cpp
+++ src/main.cpp
@@ -12,43 +12,39 @@ struct Velocity {
     Vector2 velocity;
 };
 
-struct RenderColor {
-    Color color;
+struct Sprite {
+    Texture &texture;
 };
 
+void MoveSystem(entt::registry &registry, float deltaTime);
 
-void UpdatePosition(entt::registry& registry, float deltaTime);
-
+void RenderSystem(entt::registry &registry);
 
 int main() {
     InitWindow(800, 600, "raylib");
     SetTargetFPS(60);
+    Texture texture = LoadTexture(RESOURCE_PATH"/sprite.png");
+
 
     entt::registry registry;
 
     auto entity1 = registry.create();
-    registry.emplace<Position>(entity1,  Vector2{10, 10});
+    registry.emplace<Position>(entity1, Vector2{10, 10});
     registry.emplace<Velocity>(entity1, Vector2{10, 0});
-    registry.emplace<RenderColor>(entity1 ,YELLOW);
+    registry.emplace<Sprite>(entity1, texture);
 
     auto entity2 = registry.create();
     registry.emplace<Position>(entity2, Vector2{10, 10});
     registry.emplace<Velocity>(entity2, Vector2{0, 10});
-    registry.emplace<RenderColor>(entity2 ,RED);
+    registry.emplace<Sprite>(entity2, texture);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
-        UpdatePosition(registry, GetFrameTime());
 
-        auto view = registry.view<Position, RenderColor>();
-        for (auto entity : view) {
-            Vector2 position = view.get<Position>(entity).position;
-            auto color = view.get<RenderColor>(entity).color;
+        MoveSystem(registry, GetFrameTime());
+        RenderSystem(registry);
 
-            DrawRectangle(position.x, position.y, 50, 50, color);
-
-        }
         EndDrawing();
     }
 
@@ -56,14 +52,23 @@ int main() {
     return 0;
 }
 
-void UpdatePosition(entt::registry &registry, float deltaTime) {
+void MoveSystem(entt::registry &registry, float deltaTime) {
     auto view = registry.view<Position, Velocity>();
-    for (auto entity : view) {
-        auto& position = view.get<Position>(entity);
-        auto& velocity = view.get<Velocity>(entity);
+    for (auto entity: view) {
+        auto &position = view.get<Position>(entity);
+        auto &velocity = view.get<Velocity>(entity);
 
         position.position.x += velocity.velocity.x * deltaTime;
         position.position.y += velocity.velocity.y * deltaTime;
+    }
+}
 
+void RenderSystem(entt::registry &registry) {
+    auto view = registry.view<Position, Sprite>();
+    for (auto entity: view) {
+        auto &sprite = view.get<Sprite>(entity);
+        auto &position = view.get<Position>(entity);
+
+        DrawTexture(sprite.texture, position.position.x, position.position.y, WHITE);
     }
 }
