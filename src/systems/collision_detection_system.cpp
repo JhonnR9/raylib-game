d7b782be95cb4dca03d8b46be78c15868729c0cd
jhonnr9 <4.jhones@gmail.com>
2025-08-14 17:01:00 -0300

    mutex bug fixed

--- src/systems/collision_detection_system.cpp
+++ src/systems/collision_detection_system.cpp
@@ -45,10 +45,7 @@ namespace rpg {
         return nearby_entities;
     }
 
-    void CollisionDetectionSystem::run(float dt) {
-        hash_grid_cells.clear();
-        std::unordered_set<std::pair<int, int>, pair_hash> processed_pairs;
-
+    void CollisionDetectionSystem::populate_hash_grid_cells() {
         const auto entity_view = registry->view<BoxCollider2D, Transform>();
 
         // Populate the grid on the main thread
@@ -57,80 +54,92 @@ namespace rpg {
             box_collider.is_colliding = false;
             box_collider.colliding_entities.clear();
         }
+    }
+
+    void CollisionDetectionSystem::check_collision(const std::vector<entt::entity>& entities, CollisionResult &local_collision_result) {
+        for (auto entity_a_id: entities) {
+            auto &transform_a = registry->get<Transform>(entity_a_id);
+            const auto &collider_a = registry->get<BoxCollider2D>(entity_a_id);
+
+            auto nearby_entities = get_nearby_entities(transform_a.position);
+
+            for (auto entity_b_id: nearby_entities) {
+                if (entity_a_id == entity_b_id) continue;
+
+                auto entity_pair = std::minmax(
+                    static_cast<int>(entity_a_id),
+                    static_cast<int>(entity_b_id)
+                );
+
+                if (local_collision_result.pairs.contains(entity_pair)) continue;
+
+                auto &transform_b = registry->get<Transform>(entity_b_id);
+                auto &collider_b = registry->get<BoxCollider2D>(entity_b_id);
+
+                // AABB collision
+                const float a_min_x = transform_a.position.x - (collider_a.width / 2);
+                const float a_min_y = transform_a.position.y - (collider_a.height / 2);
+                const float b_min_x = transform_b.position.x - (collider_b.width / 2);
+                const float b_min_y = transform_b.position.y - (collider_b.height / 2);
+
+                const bool is_colliding =
+                    a_min_x < b_min_x + collider_b.width &&
+                    a_min_x + collider_a.width > b_min_x &&
+                    a_min_y < b_min_y + collider_b.height &&
+                    a_min_y + collider_a.height > b_min_y;
+
+                if (is_colliding) {
+                    local_collision_result.pairs.insert(entity_pair);
+                }
+            }
+        }
+    }
+
+
+    void CollisionDetectionSystem::run(float dt) {
+        hash_grid_cells.clear();
+        populate_hash_grid_cells();
 
         // List of all cells
-        std::vector<std::pair<int, int>> all_cells;
+        std::vector<std::pair<int, int> > all_cells;
         all_cells.reserve(hash_grid_cells.size());
-        for (auto &kv: hash_grid_cells) {
-            all_cells.push_back(kv.first);
+        for (const auto &key: hash_grid_cells | std::views::keys) {
+            all_cells.push_back(key);
         }
 
-        // Local results for each thread
-        struct CollisionResult {
-            std::unordered_set<std::pair<int, int>, pair_hash> pairs;
-        };
+
         std::vector<CollisionResult> thread_results(std::thread::hardware_concurrency());
 
         // Parallel loop
-        std::for_each(
-            std::execution::par_unseq,
-            all_cells.begin(),
-            all_cells.end(),
-            [&](const auto &cell_coord) {
-                const auto thread_id = std::hash<std::thread::id>()(std::this_thread::get_id()) % thread_results.size();
-                auto &local = thread_results[thread_id];
+        std::for_each(std::execution::par_unseq, all_cells.begin(), all_cells.end(),
+                      [&](const auto &cell_coord) {
+                          const auto thread_id =
+                                  std::hash<std::thread::id>()(std::this_thread::get_id()) % thread_results.size();
+                          CollisionResult &local_collision_result = thread_results[thread_id];
 
-                const auto &entities_in_cell = hash_grid_cells[cell_coord];
-
-                for (auto entity_a_id: entities_in_cell) {
-                    auto &transform_a = registry->get<Transform>(entity_a_id);
-                    auto &collider_a = registry->get<BoxCollider2D>(entity_a_id);
-
-                    auto nearby_entities = get_nearby_entities(transform_a.position);
-
-                    for (auto entity_b_id: nearby_entities) {
-                        if (entity_a_id == entity_b_id) continue;
-
-                        auto entity_pair = std::minmax(
-                            static_cast<int>(entity_a_id),
-                            static_cast<int>(entity_b_id)
-                        );
-
-                        if (local.pairs.contains(entity_pair)) continue;
-                        local.pairs.insert(entity_pair);
-
-                        auto &transform_b = registry->get<Transform>(entity_b_id);
-                        auto &collider_b = registry->get<BoxCollider2D>(entity_b_id);
-
-                        // AABB collision
-                        float a_min_x = transform_a.position.x - (collider_a.width / 2);
-                        float a_min_y = transform_a.position.y - (collider_a.height / 2);
-                        float b_min_x = transform_b.position.x - (collider_b.width / 2);
-                        float b_min_y = transform_b.position.y - (collider_b.height / 2);
-
-                        bool is_colliding =
-                                a_min_x < b_min_x + collider_b.width &&
-                                a_min_x + collider_a.width > b_min_x &&
-                                a_min_y < b_min_y + collider_b.height &&
-                                a_min_y + collider_a.height > b_min_y;
-
-                        if (is_colliding) {
-                            // We still need to protect modifications to the registry
-                            static std::mutex reg_mutex;
-                            std::scoped_lock lock(reg_mutex);
-                            collider_a.is_colliding = true;
-                            collider_b.is_colliding = true;
-                            collider_a.colliding_entities.insert(entity_b_id);
-                            collider_b.colliding_entities.insert(entity_a_id);
-                        }
-                    }
-                }
-            }
+                          std::vector<entt::entity> &entities_in_cell = hash_grid_cells[cell_coord];
+                          check_collision(entities_in_cell, local_collision_result);
+                      }
         );
 
-        // Merge pairs
+        std::unordered_set<std::pair<int, int>, pair_hash> processed_pairs;
         for (auto &res: thread_results) {
-            processed_pairs.insert(res.pairs.begin(), res.pairs.end());
+            for (auto &pair : res.pairs) {
+                if (!processed_pairs.contains(pair)) {
+                    processed_pairs.insert(pair);
+
+                    auto entity_a_id = static_cast<entt::entity>(pair.first);
+                    auto entity_b_id = static_cast<entt::entity>(pair.second);
+
+                    auto &collider_a = registry->get<BoxCollider2D>(entity_a_id);
+                    auto &collider_b = registry->get<BoxCollider2D>(entity_b_id);
+
+                    collider_a.is_colliding = true;
+                    collider_b.is_colliding = true;
+                    collider_a.colliding_entities.insert(entity_b_id);
+                    collider_b.colliding_entities.insert(entity_a_id);
+                }
+            }
         }
 
         hash_grid_cells.clear();
--- src/systems/collision_detection_system.h
+++ src/systems/collision_detection_system.h
@@ -8,13 +8,18 @@
 #include <utility>
 #include <entt/entt.hpp>
 
-namespace rpg {
 
+namespace rpg {
     struct pair_hash {
         std::size_t operator()(const std::pair<int, int>& p) const {
             return std::hash<int>()(p.first) ^ (std::hash<int>()(p.second) << 1);
         }
     };
+    // Local results for each thread
+    struct CollisionResult {
+        std::unordered_set<std::pair<int, int>, pair_hash> pairs;
+    };
+
 
     class CollisionDetectionSystem final : public System {
         float hash_grid_cell_size{200.0f};
@@ -23,6 +28,9 @@ namespace rpg {
         std::pair<int, int> get_hash_grid_cell(float x, float y) const;
         void register_entity_in_grid(entt::entity entity, const Vector2& position);
         std::vector<entt::entity> get_nearby_entities(const Vector2& position);
+
+        void populate_hash_grid_cells();
+        void check_collision(const std::vector<entt::entity>& entities,  CollisionResult &local_collision_result);
 
     public:
         explicit CollisionDetectionSystem(entt::registry* registry);
